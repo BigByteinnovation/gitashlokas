5231ccb250ae4677473cd300e2388fa636de4b53
ankitjnvynr <ankitbkana@outlook.com>
2023-12-18 15:52:20 +0530

    data base configration set

--- upload.php
+++ upload.php
@@ -1,12 +1,5 @@
 <?php
-echo $name = $_POST['name'];
-echo $country = $_POST['country'];
-// echo $state = $_POST['state'];
-// echo $city = $_POST['city'];
-// echo $numbers = $_POST['numbers'];
-// $sql = 
-// 
-// 
+
 $status = null;
 require_once('partials\db.php');
 
