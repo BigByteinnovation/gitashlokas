ed4675f440b173283b08a390872bf96c60589461
ankitjnvynr <ankitbkana@outlook.com>
2023-12-18 14:11:52 +0530

    sdf

---
+++ upload.php
@@ -0,0 +1,9 @@
+<?php
+echo $name = $_POST['name'];
+echo $country = $_POST['country'];
+echo $state = $_POST['state'];
+echo $city = $_POST['city'];
+echo $numbers = $_POST['numbers'];
+
+$sql = 
+?>
