768ecfd5d18515f155661aca1a6633f71f77ea7b
Deepanshu <Deepanshu.yadav@brainbeeus.com>
2023-12-18 14:16:30 +0530

    slider added

--- upload.php
+++ upload.php
@@ -1,9 +1,10 @@
 <?php
 echo $name = $_POST['name'];
 echo $country = $_POST['country'];
-echo $state = $_POST['state'];
-echo $city = $_POST['city'];
-echo $numbers = $_POST['numbers'];
+// echo $state = $_POST['state'];
+// echo $city = $_POST['city'];
+// echo $numbers = $_POST['numbers'];
 
-$sql = 
-?>
+// $sql = 
+// 
+// 
