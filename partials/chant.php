645935a52180a3ed2ebff54e4205962743271e3b
ankitjnvynr <ankitbkana@outlook.com>
2023-12-19 09:37:48 +0530

    dfg

--- partials/chant.php
+++ partials/chant.php
@@ -1,25 +1,34 @@
 <?php
-$total = [];
-require_once ('db.php');
-// $sql = "SELECT SUM(numbers) as smm FROM `chant_data`";
+try {
+    $total = [];
+    require_once('db.php');
+    // $sql = "SELECT SUM(numbers) as smm FROM `chant_data`";
 // // $sql = "SELECT `numbers` FROM `chant_data`";
 // $result = mysqli_query($conn,$sql);
 // echo ($result);
 // while($row = mysqli_fetch_assoc($result)){
 //     $total = [];
-    
-// }
-// echo var_dump($totalCount);
-$sql = "SELECT SUM(numbers) AS total FROM chant_data";
-$result = $conn->query($sql);
 
-// Fetch result
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    $sum = $row['total'];
-    echo $sum;
-} else {
-    echo "No results found";
+    // }
+// echo var_dump($totalCount);
+    $sql = "SELECT SUM(numbers) AS total FROM chant_data";
+    $result = $conn->query($sql);
+
+    // Fetch result
+    if($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $sum = $row['total'];
+        echo $sum;
+        if($sum == null){
+
+            echo 0;
+        }
+    }else {
+        echo 0;
+    }
+} catch (\Throwable $th) {
+    //throw $th;
+    echo 0;
 }
 
 // Close connection
