bd968f44ed4726daa44edddf1f4004f25e3183c2
unknown <ankitbkana@outlook.com>
2023-12-19 20:47:17 +0530

    no of chants updating

---
+++ partials/pictures.php
@@ -0,0 +1,20 @@
+<?php
+$LatestPics = array();
+require_once('db.php');
+
+$sql = "SELECT `img` FROM chant_data LIMIT 10";
+$result = $conn->query($sql);
+
+// Fetch result
+if($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    while($row = $result->fetch_assoc()){
+    // echo var_dump($row);
+        array_push($LatestPics,$row['img']);
+    }
+}
+$myJSON = json_encode($LatestPics);
+// echo var_dump($myJSON);
+
+echo $myJSON;
+?>
