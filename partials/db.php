77f38d22716d64a804ba3b62676aa5340897bf3f
ankitjnvynr <ankitbkana@outlook.com>
2023-12-20 12:16:31 +0530

    dsf

---
+++ partials/db.php
@@ -0,0 +1,19 @@
+<?php
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $db = "gita_chant";
+
+$servername = "localhost";
+$username = "u704382176_GieoGita";
+$password = "GieoGita@123";
+$db = "u704382176_gita_chant";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$db);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+?>  
