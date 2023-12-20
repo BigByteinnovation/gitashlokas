418441fb6e78aedbe0f08b9240148e93e59de1a4
Deepanshu <Deepanshu.yadav@brainbeeus.com>
2023-12-20 09:35:42 +0530

    add db

--- crdb.php
+++ crdb.php
@@ -7,7 +7,7 @@ $password = "";
 $conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  die("Connectionfailed: " . $conn->connect_error);
 }
 
 // Create database
