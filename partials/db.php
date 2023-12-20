f25d5a69a535737849cfea34d331d5cbcb8911fc
Deepanshu <Deepanshu.yadav@brainbeeus.com>
2023-12-20 15:15:06 +0530

    font

--- partials/db.php
+++ partials/db.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "gita_chant";
-
 // $servername = "localhost";
-// $username = "u704382176_GieoGita";
-// $password = "GieoGita@123";
-// $db = "u704382176_gita_chant";
+// $username = "root";
+// $password = "";
+// $db = "gita_chant";
+
+$servername = "localhost";
+$username = "u704382176_GieoGita";
+$password = "GieoGita@123";
+$db = "u704382176_gita_chant";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password,$db);
